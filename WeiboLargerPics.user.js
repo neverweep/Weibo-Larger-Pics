e2349efdaa4b687271b7fedce5abc5b1c54dc125
neverweep <xia.neverweep@gmail.com>
2013-07-16 12:22:50 +0800

    加入热门、媒体版、搜索支持

--- WeiboLargerPics.user.js
+++ WeiboLargerPics.user.js
@@ -1,26 +1,30 @@
 // ==UserScript==
-// @name           Weibo Larger Pics
+// @name           Weibo Larger Pics 新浪微博之我要看大图
 // @namespace      http://xiaoxia.de/
-// @description    Easily view larger pics on Weibo.com
-// @version        0.04
+// @description    Easily view larger pics on Weibo.com 快速进入大图页面、图片详情页面和原始地址。
+// @version        0.05
 // @author         xiaoxia
 // @include        http://t.sina.com.cn/*
 // @include        http://weibo.com/*
 // @include        http://www.weibo.com/*
 // @include        http://e.weibo.com/*
-// @exclude        http://hot.weibo.com/*
-// @exclude        http://s.weibo.com/*
+// @include        http://media.weibo.com/*
+// @include        http://s.weibo.com/*
+// @include        http://hot.weibo.com/*
 // @exclude        http://weibo.com/app/*
 // @exclude        http://weibo.com/app
-// @updateinfo     增加企业版微博支持
+// @updateinfo     增加搜索、企业版、热门、媒体版支持
 // ==/UserScript==
 
 //version for auto update
-var tsinam_version = "0.04";
-var t_rdate = "2013-07-16";
+var version = "0.05";
+var modifyDate = "2013-07-16";
 
-var isFirefox = navigator.userAgent.toLowerCase().match('firefox');
-var enterprise = false;
+var isFirefox = navigator.userAgent.toLowerCase().match('firefox') != null; //判断是否为 firefox，firefox 似乎会在 dom 改变之前响应事件，别的浏览器都是在 dom 改变之后。
+var enterprise = window.location.host == 'e.weibo.com'; //判断企业版微博
+var media = window.location.host == 'media.weibo.com'; //判断媒体版微博
+var search = window.location.host == 's.weibo.com'; //判断搜索页面
+var hot = window.location.host == 'hot.weibo.com'; //判断搜索页面
 
 //判断页面类型
 if(document.getElementById('pl_content_homeFeed') != null){
@@ -28,13 +32,21 @@ if(document.getElementById('pl_content_homeFeed') != null){
     var nodeListen = document.getElementById('pl_content_homeFeed');
     entryMain(nodeListen);
 }else if(document.getElementById('pl_content_hisFeed') != null){
-    //他人时间线
-    if(window.location.host == 'e.weibo.com'){
-        //判断企业版
-        enterprise = true;
-    }
+    //他人时间线 包含企业版和普通用户
     var nodeListen = document.getElementById('pl_content_hisFeed');
     entryMain(nodeListen);
+}else if(document.getElementById('pl_weibo_feedlist') != null && search){
+    //搜索页面
+    var nodeListen = document.getElementById('pl_weibo_feedlist');
+    entryMain(nodeListen);
+}else if(document.getElementById('weibo_top_public') != null && hot){
+    //热门页面
+    var nodeListen = document;
+    entryMain(nodeListen);
+}else if(document.getElementById('epfeedlist') != null && media){
+    //媒体版页面
+    var nodeListen = document.getElementById('epfeedlist');
+    entryMedia(nodeListen);// 媒体版差异（可能）较大，使用独立的入口
 }
 
 //插入 a 标签
@@ -55,13 +67,15 @@ function insertI(node, symbol){
     node.appendChild(iElement);
 }
 
-//
+
+//主入口
 function entryMain(nodeMain){
 
-    nodeMain.addEventListener('DOMSubtreeModified',function(e){entrySub(e)});//需要监听 dom
+    nodeMain.addEventListener('DOMSubtreeModified',function(e){entrySub(e)}); //需要监听 dom
 
+    //子入口
     function entrySub(e){
-    try{
+    //try{
         var that = e.target || event.target;
 
         //console.log(that);
@@ -71,36 +85,44 @@ function entryMain(nodeMain){
 
             //火狐修正
             if(isFirefox){
-                var browserTest  = (that.className.match('WB_media_expand') != null && (that.className.match('SW_fun2') != null || that.className.match('SW_fun') != null) && that.style.display != "none") || (that.className == 'expand' && (that.parentNode.style.display != "none" && that.style.display != "none")) || (that.getAttribute('node-type') == 'imagesBox' && that.style.display != "none") || (that.className.match('comment') && that.className.match('W_textc') && that.style.display != "none");
+                var browserTest  = (that.className.match('WB_media_expand') != null && (that.className.match('SW_fun2') != null || that.className.match('SW_fun') != null) && that.style.display != "none") || (that.className == 'expand' && (that.parentNode.style.display != "none" && that.style.display != "none")) || (that.getAttribute('node-type') == 'imagesBox' && that.style.display != "none") || (that.className.match('comment') != null && that.className.match('W_textc') != null && that.style.display != "none") || (that.className.match('comment') != null && search && that.style.display != "none");
             }else{
-                var browserTest = (that.className.match('WB_media_expand') != null && (that.className.match('SW_fun2') != null || that.className.match('SW_fun') != null)) || that.className == 'expand' || that.getAttribute('node-type') == 'imagesBox' || (that.className.match('comment') && that.className.match('W_textc'));
+                var browserTest = (that.className.match('WB_media_expand') != null && (that.className.match('SW_fun2') != null || that.className.match('SW_fun') != null)) || that.className == 'expand' || that.getAttribute('node-type') == 'imagesBox' || (that.className.match('comment') != null && that.className.match('W_textc') != null) || (that.className.match('comment') != null && search);
             }
 
-            //企业版修正节点
-            if(enterprise){
-                //是否为引用图
-                var quote = (that.className == 'expand');
-                if(!quote){
-                    that = that.getElementsByClassName('expand')[0];
-                }
-            }
-
-            //判断 event dom 是否为需要操作的 dom，that.getAttribute('node-type') == 'imagesBox' 为非转发多图的状态。
+            //判断 event dom 是否为需要操作的 dom
             if(browserTest){
 
-                //判断是否为多图模式
-                var multiPics = false;
-                if(typeof(that.children[0].getAttribute('node-type')) == 'string'){
-                    that = that.children[0];
-                    multiPics = true;
+                //企业版和搜索页面修正节点
+                if(enterprise){
+                    //是否为引用图
+                    var quote = that.className == 'expand';
+                    if(!quote){
+                        that = that.getElementsByClassName('expand')[0];
+                    }
+                }else if(search){
+                    var quote = that.className == 'expand';
+                    if(!quote && that.getElementsByClassName('expand').length > 0){
+                        that = that.getElementsByClassName('expand')[0];
+                    }
+                }else if(hot){
+                    var quote = that.children.length == 1;
                 }
 
+                //判断是否为多图模式，企业版不支持发布多图真是极好的，that.getAttribute('node-type') == 'imagesBox' 为非转发多图的状态。
+                var multiPics = false;
+                if(typeof(that.children[0].getAttribute('node-type')) == 'string'){
+                    if(that.children[0].getAttribute('node-type') == 'imagesBox'){
+                        multiPics = true;
+                    }
+                    that = that.children[0];
+                }
+
+                //继续判断是否为需要操作的 dom
                 var attr = that.children[1].getAttribute('action-type');
                 var cls = that.children[1].className;
                 attr = attr != null ? attr : '';
                 cls = cls != null ? cls : '';
-
-                //继续判断是否为需要操作的 dom
                 if(attr.match('img') || cls.match('pic_list_view')){
                     var uid, pid, mid, format, para, cdn;
 
@@ -116,10 +138,25 @@ function entryMain(nodeMain){
                         that.children[0].addEventListener('mouseover',function(){
                             var format = that.children[1].children[0].children[0].children[0].children[0].src.replace(/.*(\....)$/,'$1');
                             var cdn = that.children[1].children[0].children[0].children[0].children[0].src.replace(/^.*?\/\/ww(.).*/,'$1');
-                            var para = that.children[0].getElementsByClassName('show_big')[0].getAttribute('action-data').replace(/[upm]id=/g,'').split('&');
-                            var pid = para[0];
-                            var mid = para[1];
-                            var uid = para[2];
+                            if(search){
+                                //搜索多图
+                                var para = that.children[0].getElementsByClassName('show_big')[0].href;
+                                var pid = para.replace(/.*\/pid\/(.*)\?.*/,'$1');
+                                var mid = para.replace(/.*\/mid\/(.*?)\/.*/,'$1');
+                                var uid = para.replace(/.*weibo.com\/(.*?)\/.*/,'$1');
+                            }else if(hot){
+                                //热门多图
+                                var para = that.getElementsByClassName('show_big')[0].href;
+                                var pid = para.replace(/.*\/pid\/(.*)\?.*/,'$1');
+                                var mid = para.replace(/.*\/mid\/(.*?)\/.*/,'$1');
+                                var uid = para.replace(/.*weibo.com\/(.*?)\/.*/,'$1');
+                            }else{
+                                //其他情况
+                                var para = that.children[0].getElementsByClassName('show_big')[0].getAttribute('action-data').replace(/[upm]id=/g,'').split('&');
+                                var pid = para[0];
+                                var mid = para[1];
+                                var uid = para[2];
+                            }
                             that.children[0].children[8].href = 'http://photo.weibo.com/' + uid + '/wbphotos/large/mid/' + mid + '/pid/' + pid;
                             that.children[0].children[10].href = 'http://ww' + cdn + '.sinaimg.cn/large/' + pid + format;
                         });
@@ -140,6 +177,41 @@ function entryMain(nodeMain){
                             mid = para.replace(/.*rootmid=(\d*)&?.*/,'$1');
                         }
                         pid = para.replace(/.*pid=(\w*)&?.*/,'$1');
+                    }else if(search){
+                        //搜索页面
+                        if(!quote){
+                            para = that.parentNode.parentNode.getElementsByClassName('info')[0].children[0].children[2].getAttribute('action-data');
+                            uid = para.replace(/.*uid=(\d*)&?.*/,'$1');
+                            mid = para.replace(/.*mid=(\d*)&?.*/,'$1');
+                        }else{
+                            //搜索多图
+                            if(multiPics){
+                                para = that.parentNode.parentNode.parentNode.getElementsByClassName('info')[1].children[0].children[2].getAttribute('action-data');
+                            }else{
+                                para = that.parentNode.parentNode.children[2].children[0].children[2].getAttribute('action-data');
+                            }
+                            uid = para.replace(/.*rootuid=(\d*)&?.*/,'$1');
+                            mid = para.replace(/.*rootmid=(\d*)&?.*/,'$1');
+                        }
+                        pid = para.replace(/.*pid=(\w*)&?.*/,'$1');
+                    }else if(hot){
+                        //热门页面
+                        if(!quote){
+                            para = that.parentNode.children[4].getElementsByClassName('WB_handle')[0].children[0].getAttribute('action-data');
+                            uid = para.replace(/.*uid=(\d*)&?.*/,'$1');
+                            mid = para.replace(/.*mid=(\d*)&?.*/,'$1');
+                            pid = para.replace(/.*pid=(\w*)&?.*/,'$1');
+                        }else{
+                            //热门多图
+                            if(multiPics){
+                                para = that.children[0].children[2].href;
+                                pid = para.replace(/.*\/pid\/(.*)\?.*/,'$1');
+                                mid = para.replace(/.*\/mid\/(.*?)\/.*/,'$1');
+                                uid = para.replace(/.*weibo.com\/(.*?)\/.*/,'$1');
+                            }else{
+                                //没有条件测试
+                            }
+                        }
                     }else{
                         //普通版
                         para = that.children[0].getElementsByClassName('show_big')[0].getAttribute('action-data').replace(/[upm]id=/g,'').split('&');
@@ -160,13 +232,71 @@ function entryMain(nodeMain){
 
                     //原图地址
                     insertI(that.children[0], '|');
-                    insertA(that.children[0], 'http://ww' + cdn + '.sinaimg.cn/large/' + pid + format, '大图原始地址，在此点击右键可以另存图像或复制地址', '源')
+                    insertA(that.children[0], 'http://ww' + cdn + '.sinaimg.cn/large/' + pid + format, '大图原始地址，在此点击右键可以另存图像或复制地址', '源');
                     insertI(that.children[0], '>');
                 }
             }
         }
         delete that;
 
-    }catch(err){}
+    //}catch(err){}
+    }
+}
+
+//媒体版主入口
+function entryMedia(nodeMain){
+
+    nodeMain.addEventListener('DOMSubtreeModified',function(e){entrySub(e)}); //需要监听 dom
+
+    //子入口
+    function entrySub(e){
+    //try{
+        var that = e.target || event.target;
+
+        //console.log(that);
+
+        //判断 event 节点是否符合要求
+        if(typeof(that.children) != 'undefined' && that.children.length > 0){
+
+            //火狐修正
+            if(isFirefox){
+                var browserTest = (that.className.match('MIB_assign') != null || that.className.match('blogPicOri') != null) && that.style.display != 'none';
+            }else{
+                var browserTest = that.className.match('MIB_assign') != null || that.className.match('blogPicOri') != null;
+            }
+
+            //判断 event dom 是否为需要操作的 dom
+            if(browserTest){
+                if(that.className == 'MIB_assign' && that.id.match('disp_')){
+                    that = that.children[0].children[1].children[0];
+                    var mid = that.parentNode.parentNode.parentNode.id.replace('disp_','');
+                    var uid = that.parentNode.parentNode.parentNode.parentNode.children[3].children[0].children[0].children[0].href.replace(/.*weibo.com\/(.*?)\/.*/,'$1');
+                }else if(that.className == 'blogPicOri' && that.children.length > 1){
+                    that = that.children[1];
+                    var mid = that.parentNode.id.replace('disp_','');
+                    var uid = that.parentNode.parentNode.children[0].getElementsByClassName('source_att')[0].children[0].href.replace(/.*weibo.com\/(.*?)\/.*/,'$1');
+                    var rid = that.parentNode.parentNode.children[0].getElementsByClassName('source_att')[0].children[0].children[1].getAttribute('rid'); //怎么特么又跑出来个 rid
+                    mid = rid; //用 rid 替换掉 mid，下面就不用在替换了
+                }
+
+                var format = that.children[1].src.replace(/.*(\....)$/,'$1');//图片格式
+                var cdn = that.children[1].src.replace(/^.*?\/\/ww(.).*/,'$1');
+                var pid = that.children[1].src.replace(/.*\/([\w]+)\..../,'$1');
+
+                //大图地址
+                insertI(that.children[0],'<');
+                insertA(that.children[0], 'http://photo.weibo.com/' + uid + '/wbphotos/large/photo_id/' + mid, '进入相册大图页面', '图');
+
+                //相册详情
+                insertI(that.children[0], '|');
+                insertA(that.children[0], 'http://photo.weibo.com/' + uid + '/talbum/detail/photo_id/' + mid, '进入相册详情页面', '详');
+
+                //原图地址
+                insertI(that.children[0], '|');
+                insertA(that.children[0], 'http://ww' + cdn + '.sinaimg.cn/large/' + pid + format, '大图原始地址，在此点击右键可以另存图像或复制地址', '源');
+                insertI(that.children[0], '>');
+
+            }
+        }
     }
 }
